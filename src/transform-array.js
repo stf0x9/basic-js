382e3fdfb1e0c87859caf7dcc591bb6f726577e3
stf0x9 <stf0x9@gmail.com>
2023-12-22 14:24:32 -0800

    feat: add solution transformArray

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,52 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if (!(arr instanceof Array)) {
+    throw new Error("'arr' parameter must be an instance of the Array!")
+  }
+
+  const newArr = [];
+  let prev = arr[0];
+  let skipNext = false;
+
+  for (let i = 0; i < arr.length; i++) {
+    const next = arr[i];
+
+    if (skipNext) {
+      skipNext = false;
+      continue;
+    }
+
+    const indexOfNextIsMatchingWithLoopIndex = arr.indexOf(prev) + 1 === i;
+
+    switch (next) {
+      default:
+        prev = next;
+        newArr.push(prev);
+        break;
+      case '--double-prev':
+        if (i > 0 && indexOfNextIsMatchingWithLoopIndex) {
+          newArr.push(prev);
+        }
+        break;
+      case '--discard-prev':
+        if (indexOfNextIsMatchingWithLoopIndex) {
+          newArr.pop();
+        }
+        break;
+      case '--double-next':
+        if (i < arr.length - 1) {
+          newArr.push(arr[i + 1])
+        }
+        break;
+      case '--discard-next':
+        skipNext = true;
+        break;
+    }
+
+  }
+  return newArr;
 }
 
 module.exports = {
